dcf9910dafc38018a0ece37251713761f00bc1e9
Lucas Stefano <lucassxs.sp@gmail.com>
2021-10-24 17:27:59 -0300

    add essential packages

---
+++ essential_packages.R
@@ -0,0 +1,14 @@
+# Essential Packages
+
+devtools::install_github("gadenbuie/xaringanExtra", dependencies = TRUE)
+devtools::install_github("gadenbuie/xaringanthemer", dependencies = TRUE)
+install.packages("showtext")
+devtools::install_github("hadley/emo")
+devtools::install_github("gadenbuie/countdown")
+install.packages("metathis")
+remotes::install_github("gadenbuie/applause")
+devtools::install_github("rstudio/fontawesome")
+install.packages("learnr")
+
+# DT
+install.packages("DT")
